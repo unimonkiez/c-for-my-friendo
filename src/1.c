6e4c7efe1352ed6d37d9542ae61f5177e5627861
Yuval Saraf <unimonkiez@gmail.com>
2022-11-27 13:36:48 +0200

    Do 1 without strstr

--- src/1.c
+++ src/1.c
@@ -1,25 +1,80 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+ 
+unsigned int get_times_containes(
+    const char* str,
+    const char* sub
+);
 
-typedef int bool;
-#define true 1
-#define false 0
+unsigned int get_str_size(
+    const char* str
+);
 
-
-bool contains(const char* string, const char* substring) {
-    if (strstr(string, substring) != NULL) {
-        return true;
-    }
-    return false;
-}
+void print_str(
+    const char* str
+);
 
 int main() {
-    char *sentence = "this is my sample example";
-    char *word = "sample";
+   char str1[] = "nivniv";
+   char str2[] = "niv";
 
-    if (contains(sentence, word)) {
-        printf("Contains");
-    } else {
-        printf("Nope!");
+   const unsigned int times_containes = get_times_containes(str1, str2);
+   
+   printf("The string \"");
+   print_str(str2);
+   printf("\" is contained in \"");
+   print_str(str1);
+   printf("\" %d times.\n", times_containes);
+ 
+   return 0;
+}
+
+unsigned int get_times_containes(
+    const char* str,
+    const char* sub_str
+) {
+    unsigned int res = 0;
+
+    const unsigned int str_size = get_str_size(str);
+    const unsigned int sub_size = get_str_size(sub_str);
+
+    unsigned int i = 0;
+    unsigned int j = 0;
+    while (i < str_size) {
+        const char current_char = str[i];
+        const char current_sub_char = sub_str[j];
+        if (current_char == current_sub_char) {
+            j++;
+            i++;
+            if (j == sub_size) {
+                res++;
+                j = 0;
+            }
+        } else if (j != 0) {
+            j = 0;
+        } else {
+            i++;
+        }
     }
+
+    return res;
+}
+
+void print_str(
+    const char* str
+) {
+    const unsigned int size = get_str_size(str);
+
+    for(unsigned int i = 0; i < size; i++){
+        printf("%c", str[i]);
+    }
+}
+
+unsigned int get_str_size(
+    const char* str
+) {
+    unsigned int size = 0;
+    while(str[size] != '\0') {
+        size++;
+    }
+    return size;
 }
